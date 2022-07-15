65843898a9540dff88825e7558e108956e558b7d
RibeiroBautista <95661747+RibeiroBautista@users.noreply.github.com>
2022-07-14 23:01:51 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<!DOCTYPE html><html><head><meta charset="utf-8"></head><body id="preview">
+<h1 class="code-line" data-line-start=0 data-line-end=1><a id="Sitio_Web_ECommerse_CoderHouseBackEnd31000_0"></a>Sitio Web E-Commerse (CoderHouse-BackEnd-31000)</h1>
+<hr>
+<h6 class="code-line" data-line-start=2 data-line-end=3><a id="Creado_por_Bautista_Ribeiro_2"></a>Creado por Bautista Ribeiro</h6>
+</body></html>
---
+++ src/db/database.js
@@ -0,0 +1,22 @@
+const knex = require('knex')
+
+const config = {
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "mibase",
+  },
+  pool: { min: 0, max: 7 },
+}
+const configSQLite3 = {
+  client: "sqlite3",
+  connection: { filename: "./src/db/chat/chats.sqlite" },
+  useNullAsDefault: true
+}
+
+const mysqlConnection = knex(config)
+const sqliteConnection = knex(configSQLite3)
+
+module.exports = {mysqlConnection, sqliteConnection}
